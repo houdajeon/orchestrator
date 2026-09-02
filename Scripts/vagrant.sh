8531c1d3f9e605262034430ff7597c2fd7fe4aa7
houdajeon <houdaafebts678@gmail.com>
2026-09-02 12:45:21 +0100

    update file vagrant script

--- Scripts/vagrant.sh
+++ Scripts/vagrant.sh
@@ -1,14 +1,10 @@
-# 1. Create a local bin folder if it doesn't exist
-mkdir -p ~/.local/bin
+wget https://releases.hashicorp.com/vagrant/2.4.9/vagrant_2.4.9_linux_amd64.zip
 
-# 2. Download the Vagrant zip package (v2.4.1)
-curl -LO https://releases.hashicorp.com/vagrant/2.4.1/vagrant_2.4.1_linux_amd64.zip
+unzip vagrant_2.4.9_linux_amd64.zip -d ~/.local/bin 
 
-# 3. Extract the zip archive
-unzip vagrant_2.4.1_linux_amd64.zip
+rm vagrant_2.4.9_linux_amd64.zip
 
-# 4. Move the executable to your local bin directory
-mv vagrant ~/.local/bin/
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.zshrc
+export PATH=$HOME/.local/bin:$PATH
 
-# 5. Clean up zip file
-rm vagrant_2.4.1_linux_amd64.zip
+echo "source ~/.zshrc"
